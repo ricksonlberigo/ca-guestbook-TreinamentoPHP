0b3db7963fd152272106b85094f72f2edcade977
RicksonLBerigo <ricksonl.berigo@outlook.com>
2022-03-09 09:02:40 -0300

    Finalizando projeto

--- web/index.php
+++ web/index.php
@@ -5,4 +5,4 @@ require_once __DIR__ . '/../lib/templateHandler.php';
 $config = parse_ini_file(__DIR__ . '/../config.ini');
 putenv('GUESTBOOK=' . $config['GUESTBOOK']);
 
-render('template/indexTemplate.php');
+render(__DIR__ . '/template/indexTemplate.php');
